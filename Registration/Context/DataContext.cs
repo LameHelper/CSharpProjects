6b37e81a0c0319fafc18401d70413662972f8ed8
Daniel Mechkarski <dmechkarski@transfermate.com>
2015-08-13 16:13:57 +0300

    Context folder add

---
+++ Registration/Context/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Registration.Models;
+using System.Data.Entity;
+
+namespace Registration.Controllers
+{
+    public class DataContext : DbContext
+    {
+        public IDbSet<Doctor> Doctors { get; set; }
+
+        public IDbSet<Shift> Shifts { get; set; }
+
+        public IDbSet<DoctorSchedule> Schedules { get; set; }
+    }
+}
