4f765f836d0e47c384b46d9b030ce14556c89876
daniel <daniel.mechkarski@gmail.com>
2015-08-16 23:18:28 +0300

    Shift

--- Registration/Controllers/HomeController.cs
+++ Registration/Controllers/HomeController.cs
@@ -21,7 +21,8 @@ namespace Registration.Controllers
             
         {
             ViewBag.Doctors = db.Doctors.ToList().Select(x => new SelectListItem { Text = x.DoctorName, Value = x.DoctorId.ToString() });
-            ViewBag.Shifts = db.Shifts.ToList().Select(x => new SelectListItem { Text = x.ShiftStart, Value = x.ShiftId.ToString() });
+            ViewBag.Shifts = db.Shifts.ToList().Select(x => new SelectListItem { Text = x.ShiftStart+"-"+x.ShiftEnd, Value = x.ShiftId.ToString() });
+     
             return View();
         }
 
@@ -33,12 +34,37 @@ namespace Registration.Controllers
             {
                 db.Schedules.Add(schedule);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("AddNewWorkSchedule","Home");
             }
 
            
             ViewBag.Doctors = db.Doctors.ToList().Select(x => new SelectListItem { Text = x.DoctorName, Value = x.DoctorId.ToString() });
-            ViewBag.Shifts = db.Shifts.ToList().Select(x => new SelectListItem { Text = x.ShiftStart, Value = x.ShiftId.ToString() });           
+            ViewBag.Shifts = db.Shifts.ToList().Select(x => new SelectListItem { Text = x.ShiftStart + "-" + x.ShiftEnd, Value = x.ShiftId.ToString() });
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult AddNewWorkShift()
+
+        {          
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddNewWorkShift(Shift newShift)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Shifts.Add(newShift);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+
+            ViewBag.Doctors = db.Doctors.ToList().Select(x => new SelectListItem { Text = x.DoctorName, Value = x.DoctorId.ToString() });
+            ViewBag.Shifts = db.Shifts.ToList().Select(x => new SelectListItem { Text = x.ShiftStart, Value = x.ShiftId.ToString() });
             return View();
         }
 
